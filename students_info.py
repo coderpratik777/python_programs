2975a13bc94a5d49443ddc1895e302268135de50
Pratik Pawar <121561023+coderpratik777@users.noreply.github.com>
2023-01-01 23:19:44 +0530

    Create students_info.py

---
+++ students_info.py
@@ -0,0 +1,9 @@
+class Student:
+    id=0;
+    
+    def __init__(self,name,branch):
+             self.name=name
+             self.branch=branch
+            
+student1=Student("Pratik","CSE")
+student2 = Student("Sarthak","AI ans ML")
